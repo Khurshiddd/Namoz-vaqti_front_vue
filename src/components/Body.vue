309a8801e464fb10dc1780af80678bd77271bef4
Khurshiddd <marifovxurshid80@gmail.com>
2023-07-24 11:22:01 +0500

    hg

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -5,12 +5,12 @@
             <h3 class="p-3 mb-2 bg-light text-dark">Namozgacha : 1:35:12</h3>
         </div>
         <div class="row mb-5 flex-wrap">
-            <div class="col-12 text-center vaqt">22:22 <br> Bomdod</div>
-            <div class="col-12 text-center vaqt">22:22 <br> Tong</div>
-            <div class="col-12 text-center vaqt">22:22 <br> Peshin</div>
-            <div class="col-12 text-center vaqt">22:22 <br> Asr</div>
-            <div class="col-12 text-center vaqt">22:22 <br> Shom</div>
-            <div class="col-12 text-center vaqt">22:22 <br> Xufton</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Bomdod</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Quyosh</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Peshin</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Asr</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Shom</div>
+            <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Xufton</div>
         </div>
     </div>
 </template>
@@ -25,5 +25,6 @@ export default {
     font-size: 2rem;
     margin-bottom: 2rem;
     background-color: bisque;
+    border: 3px solid rgb(142, 142, 102);
 }
 </style>
