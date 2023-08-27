566f5d4e894ea817f8e30dc5d886e16ed84fef5b
Khurshiddd <marifovxurshid80@gmail.com>
2023-08-27 14:45:29 +0500

    timer

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div>
-            <div class="text-center">
-                <h3 class="p-3 mb-2 bg-info text-dark">Hozirgi Vaqt : 9:35:12</h3>
-                <h3 class="p-3 mb-2 bg-light text-dark">Namozgacha : 1:35:12</h3>
+            <div class="text-center my-5">
+                <h3 class="p-3 mb-2 bg-info text-dark">Hozirgi Vaqt : {{ currentTime }}</h3>
+                <!-- <h3 class="p-3 mb-2 bg-light text-dark">Namozgacha : 1:35:12</h3> -->
             </div>
             <div class="row mb-5 flex-lg-nowrap m-auto par">
                 <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ times.tong_saharlik }}<br> Bomdod(saharlik)</div>
@@ -21,22 +21,35 @@ import axios from 'axios';
 export default {
     data () {
         return {
-            times: ''
+            times: '',
+            currentTime: ''
         }
     },
     name: 'Body',
     mounted(){
         this.getDay()
     },
+    created() {
+        this.timeNow();
+        setInterval(this.timeNow, 1000);
+    },
     methods: {
         async getDay() {
             try {
                 const response = await axios.get('https://islomapi.uz/api/present/day?region=Toshkent');
-                    this.times = response.data.times;
-                    console.log(this.times ); 
+                this.times = response.data.times;
+                console.log(this.times ); 
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
+        },
+        timeNow(){
+            const now = new Date();
+            const hours = String(now.getHours()).padStart(2, "0");
+            const minutes = String(now.getMinutes()).padStart(2, "0");
+            const seconds = String(now.getSeconds()).padStart(2, "0");
+            
+            this.currentTime = `${hours}:${minutes}:${seconds}`; 
         }
     }
     
