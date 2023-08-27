43ea3a814d7c22a5ce48f1735dc8fbf080d9941f
Khurshiddd <marifovxurshid80@gmail.com>
2023-08-27 13:15:05 +0500

    commit

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -5,19 +5,39 @@
                 <h3 class="p-3 mb-2 bg-info text-dark">Hozirgi Vaqt : 9:35:12</h3>
                 <h3 class="p-3 mb-2 bg-light text-dark">Namozgacha : 1:35:12</h3>
             </div>
-            <div class="row mb-5 flex-lg-nowrap m-auto par">
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Bomdod</div>
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Quyosh</div>
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Peshin</div>
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Asr</div>
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Shom</div>
-                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">22:22 <br> Xufton</div>
+            <div v-for="time in times" class="row mb-5 flex-lg-nowrap m-auto par">
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.tong_saharlik }}<br> Bomdod(saharlik)</div>
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.quyosh }}<br> Quyosh</div>
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.peshin }}<br> Peshin</div>
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.asr }}<br> Asr</div>
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.shom_iftor }}<br> Shom (iftor)</div>
+                <div class="col-md-4 col-sm-12 col-lg-2 text-center vaqt">{{ time.hufton }}<br> Xufton</div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
+    data () {
+        return {
+            times: ''
+        }
+    },
+    name: 'Body',
+    mounted(){
+        this.getDay()
+    },
+    methods: {
+        async getDay() {
+            try {
+                const response = await axios.get('https://islomapi.uz/api/present/day?region=Toshkent');
+                    this.times = response.data;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+    }
     
 }
 </script>
