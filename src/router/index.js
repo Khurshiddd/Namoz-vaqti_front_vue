62ac7d5055550faf1825f66d5aa0f7d9419a4cd9
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-16 19:34:20 +0500

    add default page bugun

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/day',
+      path: '/',
       name: 'day',
       component: () => import('@/components/Day.vue')
     },
