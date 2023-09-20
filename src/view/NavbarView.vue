2cb3440737591e97650e82ae6dca13017210da71
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-20 15:56:44 +0500

    add font-family mooli

--- src/view/NavbarView.vue
+++ src/view/NavbarView.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="">
-        <div class="bg-success py-4">
-            <h1 class="text-center">Namoz vaqtlari</h1>
-            <div class="m-auto pt-4 text-center">
-                <router-link :to="{ name: 'day' }" class="btn btn-primary">Bugun</router-link>
-                <router-link to="month" class="btn btn-primary m-2">Oylik</router-link>
-                <router-link to="year" class="btn btn-primary">Yillik</router-link>
-                <router-view></router-view>
-            </div>
-        </div>
+  <div class="">
+    <div class="bg-success py-4">
+      <h1 class="text-center">Namoz vaqtlari</h1>
+      <div class="m-auto pt-4 text-center">
+        <router-link :to="{ name: 'day' }" class="btn btn-primary">Bugun</router-link>
+        <router-link to="month" class="btn btn-primary m-2">Oylik</router-link>
+        <router-link to="year" class="btn btn-primary">Yillik</router-link>
+        <router-view></router-view>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'NavbarView'
-}
+  name: "NavbarView",
+};
 </script>
-<style>
-
-</style>
+<style></style>
